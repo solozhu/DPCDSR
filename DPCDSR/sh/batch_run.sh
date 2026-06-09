326bc2235026825b3e9563e78a91bf98e0de7d16
solozhu <37829070+solozhu@users.noreply.github.com>
2026-06-09 14:39:38 +0800

    Add files via upload

---
+++ DPCDSR/sh/batch_run.sh
@@ -0,0 +1,15 @@
+# python run.py --data_dir='Entertainment-Education' --lamb=0.6 >  results_new_new/Entertainment-Education/G/1.log
+
+# python run.py --data_dir='Entertainment-Education' --lamb=0.6 >  results_new_new/Entertainment-Education/G/2.log
+
+
+# python run.py --data_dir='Food-Kitchen' --lamb=0.8 >  results_new_new/Food-Kitchen/G/1.log
+
+# python run.py --data_dir='Food-Kitchen' --lamb=0.8 >  results_new_new/Food-Kitchen/G/2.log
+
+
+# python run.py --data_dir='Movie-Book' --lamb=0.8 >  results_new_new/Movie-Book/G/1.log
+
+python run.py --data_dir='Movie-Book' --lamb=0.8 >  results_new_new/Movie-Book/G/3.log
+
+
